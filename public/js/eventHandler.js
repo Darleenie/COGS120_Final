c614990a21f3b4a266786f3cc1a065c9ffab3409
Danlin <jiangdl0129@gmail.com>
2021-02-19 15:36:24 +0800

    submission

--- public/js/eventHandler.js
+++ public/js/eventHandler.js
@@ -14,12 +14,12 @@ function initializePage() {
 	// $('#colorBtn').click(randomizeColors);
 }
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-	document.getElementById("mySidenav").style.width = "250px";
-  }
+// /* Set the width of the side navigation to 250px */
+// function openNav() {
+// 	document.getElementById("mySidenav").style.width = "250px";
+//   }
   
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-	document.getElementById("mySidenav").style.width = "0";
-  }
+//   /* Set the width of the side navigation to 0 */
+//   function closeNav() {
+// 	document.getElementById("mySidenav").style.width = "0";
+//   }
